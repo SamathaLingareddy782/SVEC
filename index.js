90743e16767763c80a6ad37efae986c82ba7c87d
SamathaLingareddy782 <96712143+SamathaLingareddy782@users.noreply.github.com>
2021-12-31 12:41:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+ " use strict"
+// //  let name="vamcy";
+// // var age=20;
+
+// // if(true){
+//     // let name="vamcy A";
+//     //    console.log((name));
+//     // }
+
+
+
+
+
+
+
+
+
+// let employee={
+//     name: "akhila",
+//     designation:"Junoir software developer",
+//     salary:"3.64LPA",
+//     mobile:9989796959
+
+// }
+// console.log(employee)
+
+
+// let employees=["syam","raja","Akhila"]
+// console.log(employees)
+
+// 
+
+// alert("Registration is completed succesfully")
+
+
+// let number1=prompt("enter number1")
+//console.log(number1)
+
+// confirm("Are you sure to exit")
+
+// console.log("Welcome");
+// console.info("For Information");
+// console.warn("Check the code once");
+// console.error("name is not define")
+
+
+//spread operator
+
+
+// let external_marks=[23,44,67,12,89,56]
+// let internal_marks=[34,67,27,23,12,56]
+
+// let final_marks=[23,67,456,...external_marks,...internal_marks]
+
+// let results=[23,43,external_marks]
+// console.log(results)
+//console.log(final_marks)
+
+//Rest operators
+
+// function addition(x,y,...remainData){
+
+//     console.log(x)
+//     console.log(y)
+//     console.log(remainData)
+// }
+
+// console.log(addition(2,3,245,5678,123,234))
+
+//Destructuring of array
+
+
+//let employees=["sunil","Akhil","Abdul"]
+// let name1=employees[0];
+// let name2=employees[]
+
+//let [sunil,Akhil]=employees
+//console.log(sunil)
+
+
+//Destructuring of object
+
+//  let employee={
+//      uname: "akhila",
+//      designation:"Junoir software developer",
+//      salary:"3.64LPA",
+//      mobile:9989796959
+//  }
+//  let {uname,designation,...remainList}=employee;
+// console.log(uname)
+// console.log(designation)
+// console.log(remainList)
+
+
+// let demo=function(x,y){
+//     return x+y
+// }
+
+
+//arrow function
+
+// let demo= (x,y) => {
+//     return x*y
+// }
+//  console.log(demo(3,4))
+
+ let employees=["ram","pavan","shyam","Abdul"]
+ 
+// for(let i=0;i<employees.length;i++){
+//     console.log(employees[i])
+
+// for(let i in employees){
+//     console.log(i)
+// }
+
+//  for(let i of employees){
+//      console.log(i)
+//  }
+
+//map function
+
+ let result=employees.map(function(element,index){
+     return element+" k"+ index
+ }) 
+    
+ console.log(employees)
+
+  
+//    console.log(result)
